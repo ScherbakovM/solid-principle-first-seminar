ba49abeb13e9a796a349a7f80191d4b0c34d3e40
Mikhail <miwanya.spb@gmail.com>
2023-03-29 20:07:37 +0300

    final V

--- src/homework/Main.java
+++ src/homework/Main.java
@@ -6,7 +6,6 @@ import homework.util.SaverPersister;
 public class Main {
     public static void main(String[] args) {
         User user = new User("Jon");
-        User user1 = new User("Roy");
         Persister persister = new Persister(user); // копия юзер
         SaverPersister saver = new SaverPersister(persister); // инициализация класса для сохранения
         saver.saveToConsole();
